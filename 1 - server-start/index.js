3e30a763e30c46cc5acfeb41ba0f06b13a643469
Mathiasduc <mathiasduc@gmail.com>
2018-06-23 02:26:04 -0700

    First exercise

--- 1 - server-start/index.js
+++ 1 - server-start/index.js
@@ -1,1 +1,29 @@
 const { ApolloServer, gql } = require('apollo-server')
+
+const movies = [
+    {
+        name: 'test',
+    },
+    {
+        name: 'efdwde'
+    }
+]
+
+const typeDefs = gql`
+    type Query {
+        movies: [movie]
+    }
+
+    type movie {
+        id: ID,
+
+    }
+`
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers:{}
+})
+
+server.listen().then(({url})=>console.log('ready on ,'+ url))
--- 1.2 - server-exercise/index.js
+++ 1.2 - server-exercise/index.js
@@ -9,6 +9,32 @@ const frameworks = [
   {
     title: "Vue",
     git: "https://github.com/vuejs/vue/",
-    stars: 104483
+    stars: 104483  
   }
 ];
+
+
+const typeDefs = gql`
+  type Query {
+      frameworks: [framework]
+      framework(title: String): framework
+  }
+
+  type framework {
+      title: String,
+      git: String,
+      stars: Int,
+  }
+`
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers:{
+    Query:{
+      frameworks: () => frameworks,
+      framework: (_,{title})=> frameworks.find(x => x.title === title)
+    }
+  }
+})
+
+server.listen().then(({url})=>console.log('ready on ,'+ url))
