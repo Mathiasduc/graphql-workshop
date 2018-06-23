a341f82ce98fed365c0270018c7e8a6aef9a6484
Mathiasduc <mathiasduc@gmail.com>
2018-06-23 13:25:05 -0700

    end exercise 3

--- 3 - server-mutation/index.js
+++ 3 - server-mutation/index.js
@@ -4,45 +4,59 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  `postgres://${process.env.USERNAME}:${process.env.PASSWORD}@eYOUR-HOST:5432/${
-    process.env.DB
-  }`,
-  {
-    ssl: true,
-    dialectOptions: {
-      ssl: true
+  `postgres://${process.env.USERNAME}:${process.env.PASSWORD
+}@ec2-54-247-100-44.eu-west-1.compute.amazonaws.com:5432/${process.env.DB}`,
+    {
+      ssl: true,
+      dialectOptions: {
+        ssl: true
+      }
     }
-  }
-);
-
-const Framework = sequelize.define("frameworks", {
-  name: {
-    type: Sequelize.STRING
-  },
-  git: {
-    type: Sequelize.STRING
-  }
-});
-
-const typeDefs = gql`
+  );
+  
+  const Framework = sequelize.define("frameworks", {
+    name: {
+      type: Sequelize.STRING
+    },
+    git: {
+      type: Sequelize.STRING
+    }
+  });
+  
+  const typeDefs = gql`
   type Framework {
     id: String
     name: String
     git: String
   }
-
+  
   type Query {
     frameworks: [Framework]
   }
-`;
-
-const resolvers = {
-  Query: {
-    frameworks: () => Framework.findAll()
+  type Mutation {
+    addFramework(name: String, git: String): Framework
   }
-};
-const server = new ApolloServer({ typeDefs, resolvers });
-
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+  `;
+  
+  const resolvers = {
+    Query: {
+      frameworks: () => Framework.findAll()
+    },
+    Mutation: {
+      addFramework: async (_, { name, git })=> {
+        try {
+          const createdFramework = await Framework.create({ name, git });
+          return createdFramework;
+        } catch (error) {
+          console.error(error);
+          throw new Error(error);
+        }
+      }
+    }
+  };
+  const server = new ApolloServer({ typeDefs, resolvers });
+  
+  sequelize.sync().then(()=>server.listen()).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
+  
--- 5.2 - server-combine-exercise-solution/resolvers.js
+++ 5.2 - server-combine-exercise-solution/resolvers.js
@@ -2,10 +2,11 @@ const axios = require("axios");
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
+
 const sequelize = new Sequelize(
-  `postgres://${process.env.USERNAME}:${
-    process.env.PASSWORD
-  }@ec2-23-23-247-245.compute-1.amazonaws.com:5432/${process.env.DB}`,
+ `postgres://${process.env.USERNAME}:${process.env.PASSWORD}@ec2-23-23-247-245.compute-1.amazonaws.com:5432/${
+    process.env.DB}`,
+  'postgres://vynarmqiagqdch:62c6310f98ad3551d553861dca255861aa9427282628440b78567c148ec302b8@ec2-54-247-100-44.eu-west-1.compute.amazonaws.com:5432/daepo26h4s9o1p', 
   {
     ssl: true,
     dialectOptions: {
@@ -21,9 +22,9 @@ const Framework = sequelize.define("frameworks", {
   git: {
     type: Sequelize.STRING
   },
-  stars: { type: Sequelize.INTEGER, defaultValue: 0 }
-  // description: { type: Sequelize.STRING, defaultValue: "" },
-  // avatar: { type: Sequelize.STRING, defaultValue: "" }
+  stars: { type: Sequelize.INTEGER, defaultValue: 0 },
+  description: { type: Sequelize.STRING, defaultValue: "" },
+  avatar: { type: Sequelize.STRING, defaultValue: "" }
 });
 
 // When changing the DB you will need to run this with force true that will clean the DB and add the new coloumns
@@ -38,20 +39,25 @@ module.exports = {
     addFramework: async (_, { name, git }) => {
       try {
         const url = git.split("https://github.com/")[1];
-        const gh = await axios(`https://api.github.com/repos/${url}`);
-
+        const { data } = await axios(`https://api.github.com/repos/${url}`);
+        const { stargazers_count, description } = data;
+        const { owner : avatar_url } = data
         // data is at:
         // description: gh.data.description,
         //  avatar: gh.data.owner.avatar_url
+console.log(avatar_url, description);
 
         const framework = await Framework.create({
           name,
           git,
-          stars: gh.data.stargazers_count
+          stars: stargazers_count,
+          description,
+          avatar: avatar_url,
         });
 
         return framework;
       } catch (e) {
+        console.error(e)
         throw new Error(e);
       }
     }
